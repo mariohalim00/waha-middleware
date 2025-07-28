2e82a1f64819df24fa5d78d7949b3980c3bc259f
mario.halim <mario.halim@online-pajak.com>
2025-07-28 11:47:01 +0700

    add log

--- internal/util/validator.go
+++ internal/util/validator.go
@@ -1,8 +1,12 @@
 package util
 
-import "errors"
+import (
+	"errors"
+	"log"
+)
 
 func ValidateWahaInputParams(session, chatId string) error {
+	log.Printf("Validating input parameters: session=%s, chatId=%s", session, chatId)
 	if session == "" || chatId == "" {
 		return errors.New("missing session or chatId")
 	}
